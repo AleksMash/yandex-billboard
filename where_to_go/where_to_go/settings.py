acec74d53ea0c0a178aa1cb1348dab70d8251155
AleksMash <amashukov@yandex.ru>
2023-04-13 19:48:28 +0600

    Add blank start page

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -54,7 +55,7 @@ ROOT_URLCONF = "where_to_go.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -103,13 +104,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru-RU"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Omsk"
 
 USE_I18N = True
-
+USE_L10N = False
 USE_TZ = True
+DATE_FORMAT = 'd E Y'
 
 
 # Static files (CSS, JavaScript, Images)
