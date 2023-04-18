b01c71cb23228f3033d01be1a38a8151fd1f4492
AleksMash <amashukov@yandex.ru>
2023-04-18 12:55:30 +0600

    Move deployment and sensitive settings into the .env file

--- where_to_go/where_to_go/settings.py
+++ where_to_go/where_to_go/settings.py
@@ -10,8 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
 
+from environs import Env
 from pathlib import Path
+
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z-i7@88#n-)6vzs!!k6%ywz_e8!9-1bbm_q)jc5l6rx%to!(sp"
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', subcast=str)
 
 
 # Application definition
@@ -77,11 +83,9 @@ WSGI_APPLICATION = "where_to_go.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.dj_db_url('DATABASE_URL')
 }
 
 
